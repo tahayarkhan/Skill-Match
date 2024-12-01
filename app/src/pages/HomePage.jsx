92978ca44d4e954a41369853268f9fb85db69cce
Arjun Sharma <107235667+arj5@users.noreply.github.com>
2024-11-30 23:16:22 -0500

    front end home page update

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import magnify from "../assets/magnify.png"
+
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full bg-gray-100 p-4">
@@ -7,6 +9,7 @@ const Home = () => {
       <p className="text-lg text-center">
         Find volunteer opportunities that match your skills and interests!
       </p>
+      <img src={magnify} alt=""  className="m-5 h-32"/>
     </div>
   );
 };
