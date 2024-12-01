d01401d3b7e4decb4020610ff05079d3108fc40b
Arjun Sharma <107235667+arj5@users.noreply.github.com>
2024-11-30 23:39:21 -0500

    updated image on home page

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,16 +1,26 @@
-import magnify from "../assets/magnify.png"
+import volunteer from "../assets/volunteer.png"
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full bg-gray-100 p-4">
-      <h1 className="text-4xl font-bold text-center mb-4">
-        Welcome to the Volunteer Matching Platform
-      </h1>
-      <p className="text-lg text-center">
-        Find volunteer opportunities that match your skills and interests!
-      </p>
-      <img src={magnify} alt=""  className="m-5 h-32"/>
-    </div>
+    <>
+        <div className="flex flex-col items-center justify-center h-screen p-4">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-4xl font-bold mb-4">
+            Welcome to the Volunteer Matching Platform
+          </h1>
+          <p className="text-lg mb-6">
+            Find volunteer opportunities that match your skills and interests!
+          </p>
+        </div>
+        <div className="flex justify-center mt-8">
+          <img
+            src={volunteer}
+            alt="Volunteer Illustration"
+            className="h-auto w-full max-w-md"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
